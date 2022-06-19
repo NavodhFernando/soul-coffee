2b3518377000d5a3aaf1c8281fc46a4cc39e5638
Navodh Fernando <navodhfernando@Navodhs-iMac.local>
2022-06-19 23:58:58 +0530

    project create

---
+++ src/scripts/global.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+import "../lib/bootstrap/bootstrap.bundle.min.js";
+import "../lib/swiper/swiper-bundle.min.js";
+
+import "./global.js";
